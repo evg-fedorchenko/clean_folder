5cb076d2578ea08e5666d36bdb5a29756940858c
Evgen <evg.fedorchenko@gmail.com>
2023-05-21 13:33:18 +0300

    Final DZ7

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_folder',
+    version='1.0.0',
+    description='Sorted folders',
+    author='Ievgen Fedorchenko',
+    packages=find_packages()
+)
